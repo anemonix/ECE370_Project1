0670c4ae0df7662c0ceb243374b6b564730bf984
Thai Co Nguyen <anemonix@gmail.com>
2019-03-27 06:49:49 -0400

    Add files via upload

--- projectOne.ino
+++ projectOne.ino
@@ -1,113 +1,135 @@
-/*
- * All variables
- */
-int LEDPin = 13;
-int inputPin = 12; 
-int forwardPin = 10; 
-int backwardPin = 9;
-int coastClear = LOW; 
-int currentTick = 0;
-int numberOfTicks;
-int angleInput;
-int currentAngle = 0;
-float numOfRotations;
-int numberOfWhite = 0;
+#define IRA 11 
+#define IRB 12
+#define forwardPin 5
+#define backwardPin 9
 
 /*
- * Assign proper pins
+ * All of our variables
+ */
+int outerIR, innerIR;
+
+const float Kp = 4;
+float desired_angle = 0;
+float currentAngle = 0;
+float err;
+float angle;
+float velocity;
+float t1, t2;
+
+//array to hold data
+int in_data[7];
+// various descriptors for the received data
+int sign;
+int modPart;
+int divPart;
+int checksum;
+int tickCount = 0;
+
+/*
+ * Sets up serial to read and fwd/bckpin, angle and interrupt
  */
 void setup() 
 {
-  pinMode(LEDPin, OUTPUT);
+  Serial.begin(9600);
+  Serial1.begin(9600);
   pinMode(forwardPin, OUTPUT);
   pinMode(backwardPin, OUTPUT);
-  pinMode(inputPin, INPUT);
-  Serial.begin(9600);
+  pinMode(IRB, INPUT);
+  pinMode(IRA, INPUT);
+  attachInterrupt(digitalPinToInterrupt(IRA), tickInterrupt, CHANGE);
+  currentAngle = 0;
+  t1 = millis();
 }
 
 /*
- * Where all the magic happens
- * Checks for angle input, convert angle to amount of ticks
- * Spin wheel the proper direction
- * Count ticks on wheel until reach desired ticks/angle and stop
+ * Checks for angle from user and writes it as [255 255 sign(number) number/256 number%256 checksum]
+ * where each entry is one character
  */
-void loop() 
+void loop()
 {
-  if(Serial.available() > 0)
-  {
-    angleInput = Serial.parseInt();
-    //Make sure angle in in-bound
-    if((angleInput < -720) || (angleInput > 720))
+    while(true)
     {
-      Serial.print("Angle out of bound, please enter another angle between -720 and +720");
-    }
-    else
-    //Convert angle to amount of ticks!
-    {
-      Serial.print("Angle is set to: ");
-      Serial.println(angleInput);
-      numOfRotations = ((float)angleInput)/360.0;
-      numberOfTicks = (int)(numOfRotations*75);
-      if(numOfRotations != 0)
+      if(Serial1.available() >= 6)
       {
-        Serial.print("Number of ticks: ");
-        Serial.println(numberOfTicks);
+        for(int i = 0; i < 7; i++)
+        {
+          in_data[i] = (Serial1.read());
+          Serial.println(in_data[i]);
+        }
       }
+      if(in_data[0] == 255 && in_data[1] == 255)
+      {
+        sign = in_data[2];
+        divPart = in_data[3];
+        modPart = in_data[4];
+        checksum = (divPart + modPart - sign) % 255;
+        divPart = 256*divPart;
+        desired_angle = divPart + modPart;
+        if(checksum == in_data[5])
+        {
+          if(sign > 0)
+          {
+            desired_angle = abs(desired_angle);
+            //Serial.println(desired_angle);
+          }
+          else
+          {
+              desired_angle = -1 * abs(desired_angle);
+              //Serial.println(desired_angle);
+          }
+        }
+      }
+      if(desired_angle > 720)
+      {
+          desired_angle = 720;
+      }
+      else if(desired_angle < -720)
+      {
+          desired_angle = -720;
+      }
+      currentAngle = tickCount*1.2; //because 1.2 degrees per tick
+      
+        t1 = millis();
+        velocity = (Kp*(desired_angle - currentAngle)) + 127;
+        setVelocity(velocity);
+        t2 = t1;
     }
+}
+
+/*
+ * Takes care of tick interrupt so can't just put finger in front of sensor anymore
+ */
+void tickInterrupt()
+{
+  outerIR = digitalRead(IRA);
+  innerIR = digitalRead(IRB);
+
+  if(outerIR != innerIR)
+  {
+    tickCount++;
   }
+  else
+  {
+    tickCount--;
+  }  
+}
 
 
-  /*
-   * If desired number of ticks is reached then stop spinning wheels
-   * If not then keep spinning in desired direction
-   */
-  if(currentTick == abs(numberOfTicks))
-  {
-    analogWrite(forwardPin, 127);
-    analogWrite(backwardPin, 127);
-    currentTick = 0;
-    numberOfTicks = 0;
-  }
-  else if(numberOfTicks != 0)
-  {
-    if(numberOfTicks > 0)
-    {
-      Serial.println("Going forward");
-      analogWrite(backwardPin, 127);
-      analogWrite(forwardPin, 255);
-    }
-    else if(numberOfTicks < 0)
-    {
-      Serial.println("Going backward");
-      analogWrite(forwardPin, 127);
-      analogWrite(backwardPin, 255);
-    }
-  }
-
-  /*
-   * Count ticks using logic from encoder homework
-   */
-  coastClear=digitalRead(inputPin);
-  if(numberOfTicks !=0)
-  {
-    if (coastClear)
-    {
-      digitalWrite(LEDPin, LOW);
-    }
-    else
-    {
-      numberOfWhite++;
-      digitalWrite(LEDPin, HIGH);
-    }
-  }
+/*
+ * Controls which direction motor spins based on velocity
+ */
+void setVelocity(float velocityy)
+{
   
-   if(numberOfWhite == 2)
-  {
-    Serial.print("Current Ticks: ");
-    Serial.println(currentTick);
-    currentTick++;
-    numberOfWhite = 0;
+   if(velocityy >= 127) 
+   {
+    analogWrite(forwardPin, 127);
+    analogWrite(backwardPin, 2*(velocity-127));
   }
-  //Calibration
-  delay(6);
+  if(velocityy < 127) 
+  {
+    analogWrite(backwardPin, 127);
+    analogWrite(forwardPin, 2*(127-velocity));
+  }
+  return;
 }
